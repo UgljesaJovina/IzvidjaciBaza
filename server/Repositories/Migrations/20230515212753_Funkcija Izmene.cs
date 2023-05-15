79caac2d85a2e4e10071683238910f0a18e84b83
UgljesaJovina <starcevic.ugljesa@gmail.com>
2023-05-15 23:28:55 +0200

    Nova migracija

---
+++ server/Repositories/Migrations/20230515212753_Funkcija Izmene.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class FunkcijaIzmene : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clanovi_OdredskeFunkcije_OdredskaFunkcijaId",
+                table: "Clanovi");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Clanovi_OdredskaFunkcijaId",
+                table: "Clanovi");
+
+            migrationBuilder.DropColumn(
+                name: "OdredskaFunkcijaId",
+                table: "Clanovi");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "OdredskaFunkcijaId",
+                table: "Clanovi",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clanovi_OdredskaFunkcijaId",
+                table: "Clanovi",
+                column: "OdredskaFunkcijaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clanovi_OdredskeFunkcije_OdredskaFunkcijaId",
+                table: "Clanovi",
+                column: "OdredskaFunkcijaId",
+                principalTable: "OdredskeFunkcije",
+                principalColumn: "Id");
+        }
+    }
+}
